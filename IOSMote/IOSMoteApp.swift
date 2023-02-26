999f719bf75fe193272dc31ed66d716d01018965
Aditya Agrawal <adityaagrawal@Adityas-MacBook-Pro.local>
2023-02-26 21:19:23 +0800

    initial commit

---
+++ IOSMote/IOSMoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSMoteApp.swift
+//  IOSMote
+//
+//  Created by Aditya Agrawal on 26/2/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSMoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
